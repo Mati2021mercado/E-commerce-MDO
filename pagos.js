986f14f3dfbe73ae80ba14d420cfbc1a8fe0f505
Matias Mercado 02 <matymercadoone@gmail.com>
2022-03-12 13:58:06 -0300

    tratando de corregir pagos.html

--- pagos.js
+++ pagos.js
@@ -1,10 +1,7 @@
 $("#section_pagos").append(`
-
     <div class="contenedor_section-pagos">
-        
         <div >
             <i class="fa fa fa-credit-card fa-2x"></i>
-            
         </div>
         <div>
             <span>
